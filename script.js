c9496b3a27be0d411dfb962f6e1f4f03b9cd8096
Shakil Ullah <38388536+Shogunhitman@users.noreply.github.com>
2019-03-08 09:41:46 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 $(".cats").click(function(){
-    var think = $(".steps").val();
-    console.log(think);
     
-    var example ="steps" + "years";
+    var think = $(".steps").val();
+   // console.log(think);
+    
+    var create = $(".year").val();
+    //console.log(create);
 
+    var cats = $(".catto").val();
+    //console.log(cats);
+
+    
+    var fortune  = "If you took  " + think + " steps you will find " + (think*2) + " dollars on the floor. "; 
+    
+        fortune += " You will also find  " + create + " dollars in your bank account ";
+        
+        fortune += " and you  will have a " + cats + " future ";
+
+    $(".answer").text(fortune); 
+    
+    
+    
 });
+
 
